396c9e127ff7a8ebf3f2d8646ee809872d08e788
pirates73 <123927215+pirates73@users.noreply.github.com>
2024-08-17 17:17:00 +0000

    feat: initiated the project

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("Sahil","Shl"){} 
+    constructor()ERC20("sahil","sha"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
